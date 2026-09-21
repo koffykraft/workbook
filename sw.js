834025a12949c38cdb723c0654d0efb849c35c7c
koffykraft <info@koffykraft.com>
2026-09-21 21:56:25 +0530

    Cache Roastbook and Process tools

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='koffykraft-v1';
-const CORE=['./','./index.html','./roastbook.html','./roast-timer-v14.html','./roast-graph.html','./manifest.webmanifest','./koffykraft-icon.svg'];
+const CACHE='koffykraft-v2';
+const CORE=['./','./index.html','./roastbook.html','./roast-timer-v14.html','./roast-graph.html','./roasts.html','./process.html','./process-run.html','./manifest.webmanifest','./koffykraft-icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
