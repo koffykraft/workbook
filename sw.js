6bd376310a513180d4cd87a131b2bd9141b1ef57
koffykraft <info@koffykraft.com>
2026-09-21 23:12:00 +0530

    Refresh cache for Roastbooks root

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='koffykraft-v10';
-const CORE=['./','./index.html','./account.html','./brew.html','./roastbook.html','./roast-timer-v14.html','./roast-graph.html','./roasts.html','./process.html','./process-run.html','./ocr.html','./capture.html','./data.html','./privacy.html','./manifest.webmanifest','./koffykraft-icon.svg'];
+const CACHE='koffykraft-v11';
+const CORE=['./','./index.html','./roastbooks.html','./koffykraft-wordmark.png','./account.html','./brew.html','./roastbook.html','./roast-timer-v14.html','./roast-graph.html','./roasts.html','./process.html','./process-run.html','./ocr.html','./capture.html','./data.html','./privacy.html','./manifest.webmanifest','./koffykraft-icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==location.origin)return;if(e.request.mode==='navigate'){e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));return}e.respondWith(fetch(e.request).then(r=>{if(r.ok){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return r}).catch(()=>caches.match(e.request)))});
