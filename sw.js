d4b8ac5dd595c404c7f0df12951fd267d389c64b
koffykraft <info@koffykraft.com>
2026-09-21 22:25:12 +0530

    Refresh field action UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='koffykraft-v6';
+const CACHE='koffykraft-v7';
 const CORE=['./','./index.html','./roastbook.html','./roast-timer-v14.html','./roast-graph.html','./roasts.html','./process.html','./process-run.html','./ocr.html','./capture.html','./data.html','./privacy.html','./manifest.webmanifest','./koffykraft-icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
