ddfbc3f7c509958a7e7f7d85b366116b2252bbc5
koffykraft <info@koffykraft.com>
2026-09-21 22:28:36 +0530

    Refresh XLSX and home UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='koffykraft-v8';
+const CACHE='koffykraft-v9';
 const CORE=['./','./index.html','./account.html','./brew.html','./roastbook.html','./roast-timer-v14.html','./roast-graph.html','./roasts.html','./process.html','./process-run.html','./ocr.html','./capture.html','./data.html','./privacy.html','./manifest.webmanifest','./koffykraft-icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
